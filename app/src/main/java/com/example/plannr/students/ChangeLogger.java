4e4be68348352ea5f049082c52efeab98ca3368f
Nathan Lam <nath.lam@mail.utoronto.ca>
2022-11-21 19:27:59 -0500

    Added general design for students

---
+++ app/src/main/java/com/example/plannr/students/ChangeLogger.java
@@ -0,0 +1,13 @@
+package com.example.plannr.students;
+
+public class ChangeLogger {
+    boolean added;
+    String course;
+
+    public ChangeLogger(boolean added, String course) {
+        this.added = added;
+        this.course = course;
+    }
+
+    //we can add methods like get.added() if needed
+}
