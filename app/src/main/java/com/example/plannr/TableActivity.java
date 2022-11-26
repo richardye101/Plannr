62fbb9609e0e25f263c73cf093efa6e8c7160988
Nathan Lam <nath.lam@mail.utoronto.ca>
2022-11-25 21:44:39 -0500

    Began work on TableMaker

---
+++ app/src/main/java/com/example/plannr/TableActivity.java
@@ -0,0 +1,14 @@
+package com.example.plannr;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+    }
+}
