8bbcd4485e433c767342b3cc10332d561199a84d
Nathan Lam <nath.lam@mail.utoronto.ca>
2022-11-27 19:43:51 -0500

    TableMaker finished, still needs to be tested

---
+++ app/src/main/java/com/example/plannr/course/CourseCode.java
@@ -0,0 +1,23 @@
+package com.example.plannr.course;
+
+public class CourseCode {
+    Course course;
+    String code;
+
+    public CourseCode() {
+
+    }
+
+    public CourseCode(Course course, String code) {
+        this.course = course;
+        this.code = code;
+    }
+
+    public String getCourseCode() {
+        return code;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+}
