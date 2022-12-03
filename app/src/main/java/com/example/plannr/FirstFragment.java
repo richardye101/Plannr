ea7a6a8d554ad7ecce2f6eb47509509b1d08a7fc
Nathan Lam <nath.lam@mail.utoronto.ca>
2022-12-02 19:36:02 -0500

    Finished Functionality

--- app/src/main/java/com/example/plannr/FirstFragment.java
+++ app/src/main/java/com/example/plannr/FirstFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.plannr.databinding.FragmentFirstBinding;
 
@@ -35,9 +36,9 @@ public class FirstFragment extends Fragment {
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //NavHostFragment.findNavController(FirstFragment.this)
-                        //.navigate(R.id.action_FirstFragment_to_SecondFragment);
-                goToTableMaker(); //this is for testing, we can connect it later
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_tableInputFragment);
+                //goToTableMaker(); //this is for testing, we can connect it later
             }
         });
     }
