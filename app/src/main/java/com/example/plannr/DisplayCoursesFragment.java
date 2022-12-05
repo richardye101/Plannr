d7ddb541b070cc3263f0fd892aff4e626f28ada8
Matthew Xie <mattx180@gmail.com>
2022-12-04 20:59:23 -0500

    remove click functionality for student courses

--- app/src/main/java/com/example/plannr/DisplayCoursesFragment.java
+++ app/src/main/java/com/example/plannr/DisplayCoursesFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -17,26 +16,20 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.plannr.databinding.FragmentDisplayCoursesBinding;
 import com.example.plannr.course.Course;
 import com.example.plannr.course.CourseRepository;
+import com.example.plannr.databinding.FragmentDisplayCoursesBinding;
 import com.example.plannr.services.DatabaseConnection;
 
 import java.util.Map;
 
 /**
- * Generate scrollable list of buttons representing each available course.
- * Each course button takes the user to the edit course page.
+ * Generate scrollable list of buttons representing each taken course.
  */
 
 public class DisplayCoursesFragment extends Fragment {
     private FragmentDisplayCoursesBinding binding;
     private DatabaseConnection db;
-    private long pressStartTime;
-    private float pressedX;
-    private float pressedY;
-    private static final int MAX_CLICK_DURATION = 1000;
-    private static final int MAX_CLICK_DISTANCE = 15;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -157,50 +150,6 @@ public class DisplayCoursesFragment extends Fragment {
 
                 child.addView(createText(name, 20));
                 child.addView(createText(code, 15));
-
-                child.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View view, MotionEvent event) {
-                        view.getParent().requestDisallowInterceptTouchEvent(true);
-                        switch (event.getAction()) {
-                            case MotionEvent.ACTION_MOVE:
-                                child.setBackground(ContextCompat.getDrawable(
-                                        getContext(), R.drawable.course_layout_border));
-                                child.setPadding(10, 10, 10, 10);
-                                view.getParent().requestDisallowInterceptTouchEvent(false);
-                                break;
-                            case MotionEvent.ACTION_DOWN:
-                                child.setBackground(ContextCompat.getDrawable(
-                                        getContext(), R.drawable.course_layout_clicked));
-                                child.setPadding(10, 10, 10, 10);
-
-                                pressStartTime = System.currentTimeMillis();
-                                pressedX = event.getX();
-                                pressedY = event.getY();
-                                break;
-                            case MotionEvent.ACTION_UP:
-                                child.setBackground(ContextCompat.getDrawable(
-                                        getContext(), R.drawable.course_layout_border));
-                                child.setPadding(10, 10, 10, 10);
-
-                                long pressDuration = System.currentTimeMillis() - pressStartTime;
-                                if (pressDuration < MAX_CLICK_DURATION && distance(pressedX, pressedY,
-                                        event.getX(), event.getY()) < MAX_CLICK_DISTANCE) {
-
-                                    //redirect to editing page
-                                    TextView text = (TextView) child.getChildAt(1);
-                                    String code = text.getText().toString();
-
-                                    CourseRepository.setSelectedCourseId(id);
-
-                                    NavHostFragment.findNavController(DisplayCoursesFragment.this)
-                                            .navigate(R.id.action_DisplayCoursesFragment_to_adminEditFragment);
-                                }
-                                break;
-                        }
-                        return true;
-                    }
-                });
             }
         }
     }
